4e006ac0c4b89c739f8013cb47e6eba932403722
lotositsh <98526401+lotositsh@users.noreply.github.com>
2023-03-28 03:06:04 +0300

    Update check_vpn.sh

--- check_vpn.sh
+++ check_vpn.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-
-# Check if Windscribe VPN is running
-if [ "$(windscribe status | grep 'CONNECTED' | wc -l)" -eq 1 ]; then
-    echo "Windscribe VPN is already running"
+#check if Windscribe is running
+if ps -C "windscribe" > /dev/null; then
+    # Windscribe is running, check if it's connected
+    if windscribe status | grep -w "CONNECTED"; then 
+        echo "Windscribe is already connected."
+    elif windscribe status | grep -w "DISCONNECTED"; then 
+        echo "Windscribe is DISCONNECTED"; windscribe connect
+    fi 
 else
-# Start Windscribe VPN
-    windscribe connect
+    # Windscribe is not running, start it and connect
+    echo "Windscribe is not running. Starting and connecting..."
+    windscribe start && windscribe connect
 fi
